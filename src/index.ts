fe02fd5c208a14ec56191d46bd4cfe21aadd0d37
Rafes <rafadvcoliveira@gmail.com>
2022-11-20 23:43:46 -0300

    Create web sockets

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,32 @@
 import { router } from './router';
+import http from 'node:http';
 import path from 'node:path';
 import express from 'express';
 import mongoose from 'mongoose';
+import { Server } from 'socket.io';
 
-  mongoose.connect("mongodb://0.0.0.0:27017/").then(() => {
-    const app = express();
-    const port = 3001;
+const app = express();
+const server = http.createServer(app);
+export const io = new Server(server);
+mongoose.connect('mongodb://0.0.0.0:27017/').then(() => {
 
-    app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
-    app.use(express.json());
+  const port = 3001;
 
 
-    app.use(router);
-    app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`)
-    })}).catch((error) => console.log('erro', error));
+  app.use((req,res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers','*');
+
+    next();
+  });
+  app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+  app.use(express.json());
+
+
+  app.use(router);
+  server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });}).catch((error) => console.log('erro', error));
 
 
